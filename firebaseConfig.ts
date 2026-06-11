caa42b664a2c7fc08dd6ddbda32aa88993c535cc
Avishka <avishkaindumini@gmail.com>
2026-06-11 12:18:06 +0530

    security: move Firebase API keys to .env and add .env.example template

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "REDACTED_API_KEY",
-  authDomain: "wellmed-data-collection.firebaseapp.com",
-  projectId: "wellmed-data-collection",
-  storageBucket: "wellmed-data-collection.firebasestorage.app",
-  messagingSenderId: "REDACTED_SENDER_ID",
-  appId: "REDACTED_APP_ID",
-  measurementId: "REDACTED_MEASUREMENT_ID"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -18,4 +18,3 @@ const db = getFirestore(app);
 const auth = getAuth(app);
 
 export { db, auth };
-
